90c645b25be90dd415d6396f76de412cbe15a58c
Jesus Maldonado Cruz <jc236231@gmail.com>
2022-09-05 21:52:41 -0500

    Trabajo listo

--- src/HolaMundo/HolaMundo.java
+++ src/HolaMundo/HolaMundo.java
@@ -3,5 +3,6 @@ package HolaMundo;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Mi nombre es Jesus Maldonado Cruz");
     }
 }
