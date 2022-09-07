dc1e974563f3e687aee9db578be0266206c2fa8d
Jesus Maldonado Cruz <jc236231@gmail.com>
2022-09-06 19:28:44 -0500

    Trabajo listo1

--- src/Figuras/prueba.java
+++ src/Figuras/prueba.java
@@ -3,7 +3,10 @@ package Figuras;
 public class prueba {
     public static void main(String[] args) {
         Triangulo triangulo = new Triangulo (12,12,12,12,12);
-        System.out.println("El area del triangulo es " + triangulo.getArea() +
-                " y su perimetro es " + triangulo.getPerimetro());
+        Triangulo triangulo2 = new Triangulo (24,34,24,24,24);
+        System.out.println("El area del triangulo obtenida es: " + triangulo.getArea());
+        System.out.println("EL perimetro  del triangulo es: " +triangulo.getPerimetro());
+        System.out.println("El area del triangulo es: "+triangulo2.getArea()+"\nEl perimetro " +
+                "es: "+triangulo2.getPerimetro());
     }
 }
